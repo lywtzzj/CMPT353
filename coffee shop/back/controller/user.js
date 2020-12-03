8b2bc47cfab9398901ad0767b194016b42dad089
lywtzzj <lywtzzj@yahoo.com>
2020-12-03 04:06:26 -0600

    there are wrong only console log result

--- coffee shop/back/controller/user.js
+++ coffee shop/back/controller/user.js
@@ -5,11 +5,16 @@ const url = '/user';
 export default (app) => {
     app.post(`${url}/login`, (req, res) => {
         const user = req.body;
-        res.json(await getUser(user.password));
+        // res.json(getUser(user.password));
+        res.send(getUser(user.password))
     });
     app.post(`${url}/register`, (req, res) => {
         const new_user = req.body;
-        await addUser(new_user)
+        let result = res.json(addUser(new_user));
+        if (result == false) {
+            res.send("the passwors is used")
+        }
+        // console.log(result);
         res.json(getUser(new_user.password));
     });
 }
--- coffee shop/back/repository/user.js
+++ coffee shop/back/repository/user.js
@@ -1,14 +1,42 @@
+// import { response } from 'express';
 import { query } from './db.js';
 
 export const getUser = async (password) => {
-    let sql = 'SELECT * FROM user' + (password ? ` WHERE id = ${password}` : '');
-    const result = await query(sql);
-    return password ? result[0] : result;
-}
-
-export const addUser = async (menuItem) => {
     
-    await query(`INSERT INTO user (name, unitPrice) VALUES ('${menuItem.name}', '${menuItem.unitPrice}')`);
+    let sql = 'SELECT * FROM user WHERE password =' + "'"+password+"'";
+    const result = await query(sql);
+    console.log(result);
+    return result;
+    
+    // let sql = 'SELECT * FROM user' + (password ? ` WHERE password = '${password}'` : '');
+    // const result = await query(sql)
+    //     .then(function (res) {
+    //         // console.log(res);
+    //         return res.data;
+    //     }).catch(err => {
+    //         // console.log(err);
+    //     });
+    // // console.log(Promise.resolve(result) );
+    // // return password ? result[0] : result;
+}
+// export const getUser = (password) => {
+//     return new Promise((resolve, reject) => {
+
+//         let sql = 'SELECT * FROM user' + (password ? ` WHERE password = '${password}'` : '');
+//         query(sql,(err, result) =>{
+//             if (err) {
+//                 reject(err);
+//             }else{return resolve(password ? result[0] : result)}
+//         });
+//     })
+// }
+
+export const addUser = async (user) => {
+    let test = await getUser(user.password);
+    // console.log(test);
+    if (test === null) {
+        await query(`INSERT INTO user (username, password, name, role) VALUES ('${user.username}', '${user.password}','${user.name}', '${user.role}')`);
+    } else { return false }
 };
-const sql = 'CREATE TABLE IF NOT EXISTS user (id int NOT NULL AUTO_INCREMENT, username VARCHAR(50), password VARCHAR(50), name VARCHAR(50), role VARCHAR(50) , PRIMARY KEY (id))';
+const sql = 'CREATE TABLE IF NOT EXISTS `user` (id int NOT NULL AUTO_INCREMENT, username VARCHAR(50), password VARCHAR(50), name VARCHAR(50), role VARCHAR(50) , PRIMARY KEY (id))';
 query(sql);
