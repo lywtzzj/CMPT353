2e4c83cfa01dc71b05d64ceaba9998c16a514c07
lywtzzj <lywtzzj@yahoo.com>
2020-12-03 03:02:53 -0600

    order is done by my chance

--- coffee shop/back/controller/order.js
+++ coffee shop/back/controller/order.js
@@ -1,13 +1,24 @@
+import { getOrder, addOrder, sortOrder, readyOrder } from "../repository/order.js";
 const url = '/order';
 
 export default (app) => {
-    app.get(url, (req, res) => {
-        res.send('hello world');
+    app.get(url, async (req, res) => {
+        let infor = req.body;
+        res.json(await getOrder(infor));
+        res.send('get');
     });
-    app.post(url, (req, res) => {
-        res.send('hello world');
+    app.post(url, async (req, res) => {
+        let new_order = req.body;
+        await addOrder(new_order);
+        res.send('add');
     });
-    app.put(url, (req, res) => {
+    app.put(`${url}/sort`, async (req, res) => {
+        res.json(await sortOrder());
+        res.send('staff sort');
+    });
+    app.put(`${url}/ready`, async (req, res) => {
+        let ready = req.body;
+        res.json(await readyOrder(ready));
         res.send('put menu');
     });
 }
---
+++ coffee shop/back/repository/order.js
@@ -0,0 +1,26 @@
+import { query } from './db.js';
+
+export const getOrder = async (infor) => {
+    // let sql = 'SELECT * FROM `order`' + (id ? ` WHERE userId = ${id}` : '');
+    let sql = " SELECT * FROM `order` WHERE userId = " + infor.userId+" ORDER BY time";
+    const result = await query(sql);
+    console.log(result);
+    return result;
+    // return infor.userId ? result[-1] : result;
+}
+
+export const addOrder = async (add) => {
+    let addO = ("INSERT INTO order (userId, time, ready, cancelled, content) VALUES ('" + add.userId + "', '" + new Date().toISOString() + "', 'not', 'not', '" + add.list + "');");
+    console.log(addO);
+    return await query("INSERT INTO `order` (userId, time, ready, cancelled, content) VALUES ('" + add.userId + "', '" + new Date().toISOString() + "', 'not', 'not', '" + add.list + "');");
+};
+
+export const sortOrder = async () => {
+    return await query("SELECT * FROM `order` ORDER BY time");
+}
+export const readyOrder = async (ready) => {
+    await query("UPDATE `order` SET ready = 'yes' WHERE id = "+ready.id);
+};
+const sql = 'CREATE TABLE IF NOT EXISTS `order` (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, userId VARCHAR(50), time VARCHAR(50), ready VARCHAR(50), cancelled VARCHAR(50), content VARCHAR(250))';
+// const sql = "CREATE TABLE IF NOT EXISTS 'order' ('id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 'userid' VARCHAR(50), 'timestamp' VARCHAR(50), 'ready' VARCHAR(50), 'cancelled' VARCHAR(50), 'content' VARCHAR(250))";
+query(sql);
